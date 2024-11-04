ceca37104bab0636491d77c0e560cea702efa2dc
deji100 <adeyemideji9@gmail.com>
2024-11-04 13:10:43 +0100

    updated Debug Mode

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bw74ai$k5+a*)s8ro$g%%&w8y2ouzgr_q2(^y+rr+&mldzxlk2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
